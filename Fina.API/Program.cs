3645bf2b4617f21297baf8f5ce6a064ee8687e2e
ArleiRodrigues1 <Arleisp5365@gmail.com>
2024-06-03 19:45:48 -0300

    Second

--- Fina.API/Program.cs
+++ Fina.API/Program.cs
@@ -1,5 +1,12 @@
+using Fina.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+const string connectionString = "Server=localhost,1433;Database=Fina;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False;TrustServerCertificate=True;";
+
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(connectionString));
 
 app.MapGet("/", () => "Hello World!");
 
